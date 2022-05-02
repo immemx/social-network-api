1ee82e23a5fff70b2e1f7e6691f3b8c00ad3f1b5
immemx <eayresm4@gmail.com>
2022-05-01 21:43:54 -0400

    user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,28 @@
 const router = require('express').Router();
 const {
-}
+    getAllUsers,
+    createUser,
+    getUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller')
+
+router
+.route('/')
+.post(createUser)
+.get(getAllUsers)
+
+router
+.route('/:id')
+.get(getUser)
+.put(updateUser)
+.delete(deleteUser)
+
+router
+.route('/:userId/friends/:friendId')
+.put(addFriend)
+.delete(removeFriend)
+
+module.exports = router;
